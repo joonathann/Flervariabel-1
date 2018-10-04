657e5899d592e2bc6ed1f45abfec2395317764b1
Jonathan <jonathanpetersson@Jonathans-MacBook-Pro.local>
2018-10-04 14:41:07 +0200

    Final version

--- exc2.py
+++ exc2.py
@@ -20,7 +20,7 @@ class Planet:
 		self.y += self.vy * dt
 		self.t += dt #total time elapsed
 	
-	def orbitPlot(self,stepSize,time, plot = True): #method to simplily ploting
+	def orbitPlot(self,stepSize,time): #method to simplily ploting
 		self.xlist = [self.x]
 		self.ylist = [self.y]
 		
@@ -29,14 +29,13 @@ class Planet:
 			self.xlist.append(self.x)
 			self.ylist.append(self.y)
 		
-		if plot == True:
-			plt.plot(self.xlist,self.ylist)
-			plt.plot(0,0,'r*')
-			plt.axis('equal')
-			
-		elif plot == False: 
-			return xlist
-
+		#ploting and formatin
+		plt.plot(self.xlist,self.ylist)
+		plt.plot(0,0,'r*')
+		plt.axis('equal')
+		plt.legend()
+		self.getPeriodAndAxis(stepSize) #Calculates and prints SMA and period for every orbit.
+		
 	def getPeriodAndAxis(self,dt):
 		SMA = (abs(min(self.xlist)) + abs(max(self.xlist)))/2  #Semi Major Axis
 		stepnr = 0  #stepcounter starting value
@@ -50,5 +49,36 @@ class Planet:
 		print(2*stepnr*dt)  #double stepnumber times stepsize is the period
 		print('SMA is ')
 		print(SMA)
-		print((2*stepnr*dt)**2/SMA**3)   #Keplers Law
+		print('T^2/a^3=')
+		print((2*stepnr*dt)**2/SMA**3)   #Numerical aroximation of Keplers third Law
+#%%
 		
+print("Diffrent orbits starting at the same point but with different initial velocity (in the y direction)")
+ex1 = Planet(5000,0,0,0.03)
+ex1.orbitPlot(2,500000)
+
+ex2 = Planet(5000,0,0,0.075)
+ex2.orbitPlot(2,500000)
+
+ex3 = Planet(5000,0,0,0.1)
+ex3.orbitPlot(2,700000)
+
+ex4 = Planet(5000,0,0,0.11)
+ex4.orbitPlot(2,1200000)
+#%%
+print("Same orbits but change in dt")
+print("dt = 1")
+ex1 = Planet(5000,0,0,0.075)
+ex1.orbitPlot(1,500000)
+
+print("dt = 100")
+ex2 = Planet(5000,0,0,0.075)
+ex2.orbitPlot(100,500000)
+
+print("dt = 10000")
+ex3 = Planet(5000,0,0,0.075)
+ex3.orbitPlot(10000,1500000)
+
+print("dt = 50000")
+ex4 = Planet(5000,0,0,0.075)
+ex4.orbitPlot(50000,700000)
